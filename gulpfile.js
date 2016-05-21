7b99ea5d4204640c0d170131a937fd2539adc423
Roy A Vanegas <roy@thecodeeducators.com>
2016-05-21 08:16:49 -0400

    Remove comment bodies

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,6 @@ var browserSpecificPrefixGenerator = require('gulp-autoprefixer');
 
 /**
  * COMPILE CSS
- *
- * 1. Use `main.scss` as the source for all the following tasks.
- * 2. Toss errors to the command line, highlighting the word “Error” in red.
- * 3. Compress the output that Sass generates from the file `main.scss`.
- * 4. Carry out to ten places the mathematical precision of all floating-point
- *    mathematical calculations carried out in `main.scss`.
- * 5. Using data from the web site caniuse.com, prefix any CSS3 properties that
- *    require a browser-specific prefix for as far back as two versions of browsers.
- * 6. Lastly, write all the output to the implied file `main.css` in the directory
- *    `css`.
  */
 gulp.task('compileCSS', function () {
     gulp.src('sass/style.scss')
@@ -32,7 +22,5 @@ gulp.task('compileCSS', function () {
 
 /**
  * DEFAULT
- *
- * Invoke the default task by simply running “gulp” at the command line.
  */
 gulp.task('default', ['compileCSS']);
