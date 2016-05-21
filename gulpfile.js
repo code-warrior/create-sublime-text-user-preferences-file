7926625938ace9fb67d41a807996cce7cb6e6c55
Roy A Vanegas <roy@thecodeeducators.com>
2016-05-21 17:27:24 -0400

    Replace hard-coded values with object literals

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,35 @@
 
 var gulp = require('gulp'),
     CSSPreprocessor = require('gulp-sass'),
-    browserSpecificPrefixGenerator = require('gulp-autoprefixer');
+    browserSpecificPrefixGenerator = require('gulp-autoprefixer'),
+    filenames = {
+        styles: 'style.scss'
+    },
+    baseFolders = {
+        source: {
+            styles: {
+                sass: 'sass/'
+            }
+        },
+        target: {
+            styles: {
+                css: 'css/'
+            }
+        }
+    };
 
 /**
  * COMPILE CSS
  */
 gulp.task('compileCSS', function () {
-    gulp.src('sass/style.scss')
+    gulp.src(baseFolders.source.styles.sass + filenames.styles)
         .pipe(new CSSPreprocessor().on('error', CSSPreprocessor.logError))
         .pipe(new CSSPreprocessor({
             outputStyle: 'compressed',
             precision: 10
         }))
         .pipe(browserSpecificPrefixGenerator({browsers: ['last 2 versions']}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest(baseFolders.target.styles.css));
 });
 
 /**
